296c0388090c19f92fb1ff8c1835024150f1ca92
Jonas Leonhard <54074887+JonasLeonhard@users.noreply.github.com>
2023-07-04 21:02:55 +0200

    Update fm-nvim.lua

--- lua/fm-nvim.lua
+++ lua/fm-nvim.lua
@@ -215,6 +215,7 @@ end
 function M.Xplr(dir)
     dir = dir or "."
     if config.ui.default == "float" then
+        print(config.cmds.xplr_cmd .. " | save --raw -f /tmp/fm-nvim " .. dir)
         createWin(config.cmds.xplr_cmd .. " | save --raw -f /tmp/fm-nvim " .. dir, "<CR>")
     elseif config.ui.default == "split" then
         createSplit(config.cmds.xplr_cmd .. " | save --raw -f /tmp/fm-nvim " .. dir, "<CR>")
